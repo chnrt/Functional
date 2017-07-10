b3c8ba08f206066fd74e3bce609747e84e9a8993
IMustTo <cxjbinfo@gmail.com>
2017-07-10 16:05:38 +0800

    compose practice

--- practice/compose.js
+++ practice/compose.js
@@ -33,3 +33,41 @@ const _underscore = _.replace(/\W+/g, '_'); //<-- 无须改动，并在 sanitize
 
 const sanitizeNames = _.map(_.compose(_underscore, _.toLower));
 console.log(sanitizeNames(["Hello World"]));
+
+// 彩蛋 1:
+// ============
+// 使用 compose 重构 availablePrices
+
+// const availablePrices = function(cars) {
+//   var available_cars = _.filter(_.prop('in_stock'), cars);
+//   return available_cars.map(function(x){
+//     return accounting.formatMoney(x.dollar_value);
+//   }).join(', ');
+// };
+const availablePrices = _.compose(
+  _.join(', '),
+  _.map(_.prop('dollar_value')),
+  _.filter(_.prop('in_stock'))
+);
+console.log(availablePrices(CARS));
+
+// 彩蛋 2:
+// ============
+// 重构使之成为 pointfree 函数。提示：可以使用 _.flip()
+// var fastestCar = function(cars) {
+//   var sorted = _.sortBy(function(car){ return car.horsepower }, cars);
+//   var fastest = _.last(sorted);
+//   return fastest.name + ' is the fastest';
+// };
+// var trace = _.curry(function(tag, x){
+//   console.log(tag, x);
+//   return x;
+// });
+const fastestCar = _.compose(
+  _.join(''),
+  _.flip((a, b) => [a, b])(' is the fastest'),
+  _.prop('name'),
+  _.last,
+  _.sortBy(_.prop('horsepower'))
+);
+console.log(fastestCar(CARS));
