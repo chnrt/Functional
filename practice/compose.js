b03db67b764713a47d2e6426d1c42ba0c009760c
IMustTo <cxjbinfo@gmail.com>
2017-07-10 15:46:43 +0800

    compose practice

--- practice/compose.js
+++ practice/compose.js
@@ -10,7 +10,26 @@ var CARS = [
     {name: "Pagani Huayra", horsepower: 700, dollar_value: 1300000, in_stock: false}
   ];
 
-
+// 1
 const isLastInStock = _.compose(_.prop('in_stock'), _.last);
-
 console.log(isLastInStock(CARS));
+
+// 2
+const getFirstCarName = _.compose(_.prop('name'), _.head());
+console.log(getFirstCarName(CARS));
+
+// 练习 3:
+// ============
+// 使用帮助函数 _average 重构 averageDollarValue 使之成为一个组合
+const _average = xs => _.reduce(_.add, 0, xs) / xs.length; // <- 无须改动
+
+const averageDollarValue = _.compose(_average, _.map(_.prop('dollar_value')));
+console.log(averageDollarValue(CARS));
+
+// 练习 4:
+// ============
+// 使用 compose 写一个 sanitizeNames() 函数，返回一个下划线连接的小写字符串：例如：sanitizeNames(["Hello World"]) //=> ["hello_world"]。
+const _underscore = _.replace(/\W+/g, '_'); //<-- 无须改动，并在 sanitizeNames 中使用它
+
+const sanitizeNames = _.map(_.compose(_underscore, _.toLower));
+console.log(sanitizeNames(["Hello World"]));
